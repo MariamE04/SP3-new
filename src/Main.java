28c7d36b6fefd4210ff074f9d51788202b900d9a
Mariam <elmirmariam16@gmail.com>
2024-11-21 17:40:42 +0100

    ændring i fileIO (ikke færdig)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static ArrayList<User> users;
+    private static ArrayList<Media> media;
     public static void main(String[] args) {
+    }
+
+
+    public void setup(){
+
     }
 }
