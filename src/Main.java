fd8e17738fc3e2678d6d0b69a458f38a848ab1b6
Mariam <elmirmariam16@gmail.com>
2024-11-22 09:47:28 +0100

    textUI laves

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static ArrayList<User> users;
-    private static ArrayList<Media> media;
     public static void main(String[] args) {
-    }
-
-
-    public void setup(){
 
     }
 }
